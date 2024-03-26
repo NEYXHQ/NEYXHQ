b2774670123b5afc3b1477711a1e41f17a7b3560
AltoPal0 <stephane@neyx.network>
2024-03-26 16:55:42 +0100

    Add mint base

---
+++ vanilla-NEYX/x/mint/simulation/params.go
@@ -0,0 +1,76 @@
+package simulation
+
+// DONTCOVER
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+
+	"github.com/ignite/modules/x/mint/types"
+)
+
+const (
+	keyInflationRateChange     = "InflationRateChange"
+	keyInflationMax            = "InflationMax"
+	keyInflationMin            = "InflationMin"
+	keyGoalBonded              = "GoalBonded"
+	keyDistributionProportions = "DistributionProportions"
+	keyFundedAddresses         = "FundedAddresses"
+)
+
+// ParamChanges defines the parameters that can be modified by param change proposals
+// on the simulation
+func ParamChanges() []simtypes.LegacyParamChange {
+	return []simtypes.LegacyParamChange{
+		simulation.NewSimLegacyParamChange(types.ModuleName, keyInflationRateChange,
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenInflationRateChange(r))
+			},
+		),
+		simulation.NewSimLegacyParamChange(types.ModuleName, keyInflationMax,
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenInflationMax())
+			},
+		),
+		simulation.NewSimLegacyParamChange(types.ModuleName, keyInflationMin,
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenInflationMin())
+			},
+		),
+		simulation.NewSimLegacyParamChange(types.ModuleName, keyGoalBonded,
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenGoalBonded())
+			},
+		),
+		simulation.NewSimLegacyParamChange(types.ModuleName, keyDistributionProportions,
+			func(r *rand.Rand) string {
+				proportions := GenDistributionProportions(r)
+				return fmt.Sprintf(
+					`{"staking":"%s","funded_addresses":"%s","community_pool":"%s"}`,
+					proportions.Staking.String(),
+					proportions.FundedAddresses.String(),
+					proportions.CommunityPool.String(),
+				)
+			},
+		),
+		simulation.NewSimLegacyParamChange(types.ModuleName, keyFundedAddresses,
+			func(r *rand.Rand) string {
+				weightedAddrs := GenFundedAddresses(r)
+				weightedAddrsStr := make([]string, 0)
+				for _, wa := range weightedAddrs {
+					s := fmt.Sprintf(
+						`{"address":"%s","weight":"%s"}`,
+						wa.Address,
+						wa.Weight.String(),
+					)
+					weightedAddrsStr = append(weightedAddrsStr, s)
+				}
+				return fmt.Sprintf("[%s]", strings.Join(weightedAddrsStr, ","))
+			},
+		),
+	}
+}
---
+++ vanilla-NEYX/x/mint/types/keys.go
@@ -0,0 +1,12 @@
+package types
+
+// MinterKey is the key to use for the keeper store.
+var MinterKey = []byte{0x00}
+
+const (
+	// module name
+	ModuleName = "mint"
+
+	// StoreKey is the default store key for mint
+	StoreKey = ModuleName
+)
